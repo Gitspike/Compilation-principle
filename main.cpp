ad51d4b94c2aac0aad8b86075447d21b0dbd7e76
huxiaolong <hxl814446051@bupt.edu.cn>
2022-04-10 11:53:51 +0000

    Original main.cpp without overriding

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+#include "antlr4-runtime.h"
+#include "PascalSLexer.h"
+#include "PascalSParser.h"
+#include "PascalSBaseListener.h"
+
+using namespace antlr4;
+
+int main(int agrc, char* argv[]){
+	std::ifstream stream(argv[1]);
+	ANTLRInputStream input(stream);
+	// Create antlr lexer
+	PascalSLexer lexer(&input);
+	CommonTokenStream tokens(&lexer);
+	
+	// Create antlr parser
+	PascalSParser parser(&tokens);
+
+	// Create parser tree and set initial rule
+	tree::ParseTree *tree = parser.program();
+	PascalSBaseListener listener;
+	//ArrayInitBaseListener listener;
+	tree::ParseTreeWalker::DEFAULT.walk(&listener, tree);
+
+	return 0;
+}
